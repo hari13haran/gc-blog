53aeaeb47f7df269dd44107788a1db7a2f647d61
hari13haran <h.hariran@gmail.com>
2022-07-24 22:11:30 +0530

    Injecting dynamic values in jsx templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to my new Blog!';
+  const likes = 50;
+  const link = "https://www.google.co.in"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>Avg: { Math.random() * likes }</p>
+        <a href="link">Google Home page</a>
       </div>
     </div>
   );
