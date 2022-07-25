8490c3e2f14d4de4cfd35d33e485f3de26f03790
hari13haran <h.hariran@gmail.com>
2022-07-26 00:21:52 +0530

    added button onClick events - example

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello there!')
+    }
+
+    // event property is a default property that can be passed & accessed
+    const handleClickArgument = (name, event) => {
+        console.log('Hello, '+ name, event.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(event) => {
+                handleClickArgument('hari', event)
+            }}>Click Name</button>
         </div>
      );
 }
