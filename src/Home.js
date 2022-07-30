43c6cc70399dc931ed1bb8076e8a2bdf7649f9cb
hari13haran <h.hariran@gmail.com>
2022-07-31 00:52:36 +0530

    Redering list of divs with js logic for json data

--- src/Home.js
+++ src/Home.js
@@ -2,27 +2,42 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('Hari');
-    const [age, setAge] = useState(25)
+    // const [name, setName] = useState('Hari');
+    // const [age, setAge] = useState(25)
 
-    const handleClick = () => {
-        setName('GC');
-        setAge(27);
-    }
+    // const handleClick = () => {
+    //     setName('GC');
+    //     setAge(27);
+    // }
 
     // event property is a default property that can be passed & accessed
     // const handleClickArgument = (name, event) => {
     //     console.log('Hello, '+ name, event.target);
     // }
 
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
+
+            {/* <h2>Home Page</h2>
             <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
+            <button onClick={handleClick}>Click me</button> */}
             {/* <button onClick={(event) => {
                 handleClickArgument('hari', event)
             }}>Click Name</button> */}
+
+            { blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
+
         </div>
      );
 }
