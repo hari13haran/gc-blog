cab9afd3038f2a96dc407fd6e11248fbcfb87ac8
hari13haran <h.hariran@gmail.com>
2022-07-31 19:53:15 +0530

    Props for components + filter props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
@@ -31,12 +32,9 @@ const Home = () => {
                 handleClickArgument('hari', event)
             }}>Click Name</button> */}
 
-            { blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by: {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs" />
+
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" />
 
         </div>
      );
