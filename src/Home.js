0ba606fb8e0b47d46d2bc30d8cd46e66fa51df07
hari13haran <h.hariran@gmail.com>
2022-07-31 23:20:53 +0530

    JSON server to simulate fetch data + loading msg

--- src/Home.js
+++ src/Home.js
@@ -16,23 +16,34 @@ const Home = () => {
     //     console.log('Hello, '+ name, event.target);
     // }
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    // const [blogs, setBlogs] = useState([
+    //     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    //     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    //     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    // ]);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
-    const [name, setName] = useState('mario');
+    const [isPending, setIsPending] = useState(true);
+
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter((blog) => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // }
+
+    // const [name, setName] = useState('mario');
 
     useEffect(() => {
-        console.log('use effect ran!');
-        console.log('Name:'+ name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            setBlogs(data);
+            setIsPending(false);
+        })
+    }, []);
 
     return ( 
         <div className="home">
@@ -44,13 +55,15 @@ const Home = () => {
                 handleClickArgument('hari', event)
             }}>Click Name</button> */}
 
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            { isPending && <div>Loading...</div> }
+
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
 
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" /> */}
 
             {/* useEffect hook example */}
-            <button onClick={() => setName('hari')}>Change Name</button>
-            <p>{name}</p>
+            {/* <button onClick={() => setName('hari')}>Change Name</button>
+            <p>{name}</p> */}
 
         </div>
      );
