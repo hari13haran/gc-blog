c90e8f4419119652d5f29c221ba744f8177728e0
hari13haran <h.hariran@gmail.com>
2022-08-02 00:04:14 +0530

    Route parameters + added Links to blogs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   // const title = 'App Component';
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         {/* <h1>{ title }</h1>
