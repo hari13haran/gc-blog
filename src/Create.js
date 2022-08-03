945d7f9853d32cca0f24e0ce2011e5cb37a629ba
hari13haran <h.hariran@gmail.com>
2022-08-04 00:26:52 +0530

    Create Form + Submit event

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('hari');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const blog = { title, body, author };
+        console.log(blog);
+    }
+
     return (  
        <div className="create">
         <h2>Add a new blog</h2>
+        <form onSubmit={handleSubmit}>
+            <label>Blog title:</label>
+            <input type="text" 
+                   required
+                   value={title}
+                   onChange={(event) => setTitle(event.target.value)}
+            />
+            <label>Blog body:</label>
+            <textarea 
+                   required
+                   value={body}
+                   onChange={(event) => setBody(event.target.value)}
+            ></textarea>
+            <label>Blog author:</label>
+            <select
+                value={author}
+                onChange={(event) => setAuthor(event.target.value)}
+            >
+                <option value="hari">Hari</option>
+                <option value="haran">Haran</option>
+            </select>
+            <button>Add Blog</button>
+        </form>
        </div> 
     );
 }
